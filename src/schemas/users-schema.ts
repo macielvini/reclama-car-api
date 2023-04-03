487593aaf8b2f572e7376abdfbbd7fd7f49d5814
Vinicius Maciel <vm.viniciusmaciel@gmail.com>
2023-04-03 20:48:56 -0300

    fix: added name to schema

--- src/schemas/users-schema.ts
+++ src/schemas/users-schema.ts
@@ -3,5 +3,6 @@ import { z } from "zod";
 export const userSchema = z.object({
   password: z.string().min(6).max(50),
   email: z.string().email(),
+  name: z.string().max(50),
   image: z.string().url(),
 });
