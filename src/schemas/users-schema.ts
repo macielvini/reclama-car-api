54256ad1c87132b078107b8c14494dacbb25d0f8
Vinicius Maciel <vm.viniciusmaciel@gmail.com>
2023-04-03 21:04:19 -0300

    fix: schema image type validation

--- src/schemas/users-schema.ts
+++ src/schemas/users-schema.ts
@@ -3,6 +3,11 @@ import { z } from "zod";
 export const userSchema = z.object({
   password: z.string().min(6).max(50),
   email: z.string().email(),
-  name: z.string().max(50),
-  image: z.string().url(),
+  name: z.string().max(60),
+  image: z
+    .string()
+    .url()
+    .endsWith("jpg" || "jpeg" || "png", {
+      message: "Invalid url or image type. Use jpg, jpeg or png!",
+    }),
 });
