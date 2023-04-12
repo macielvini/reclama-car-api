fad5906adb10fdd2f024bb4e82c07fb81018e8f2
Vinicius Maciel <vm.viniciusmaciel@gmail.com>
2023-04-11 21:05:40 -0300

    feat: find repository

--- src/repositories/reviews-repository.ts
+++ src/repositories/reviews-repository.ts
@@ -28,4 +28,40 @@ async function create(userId: string, { tags, ...data }: CreateReviewParams) {
   });
 }
 
-export const reviewsRepository = { create };
+async function findAll() {
+  return await prisma.review.findMany({
+    include: {
+      _count: { select: { Reaction: true } },
+      car: { include: { manufacture: true } },
+      TagsOnReviews: { include: { tag: true } },
+    },
+  });
+}
+
+async function findTrending(take: number) {
+  const last30days = dayjs().subtract(30, "d").toDate();
+  const last15days = dayjs().subtract(15, "d").toDate();
+
+  return prisma.review.findMany({
+    where: {
+      createdAt: { gte: last30days },
+      AND: { Reaction: { some: { createdAt: last15days } } },
+    },
+    orderBy: { Reaction: { _count: "desc" } },
+    take: take,
+  });
+}
+
+async function findTopReactions() {
+  return prisma.review.findMany({
+    include: { _count: { select: { Reaction: true } } },
+    orderBy: { Reaction: { _count: "desc" } },
+  });
+}
+
+export const reviewsRepository = {
+  create,
+  findAll,
+  findTrending,
+  findTopReactions,
+};
