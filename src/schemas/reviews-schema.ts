aa16cd905ff86fc0eb30dea8eb572e4683d94fc4
Vinicius Maciel <vm.viniciusmaciel@gmail.com>
2023-04-14 19:49:28 -0300

    refactor: review schema with rating

--- src/schemas/reviews-schema.ts
+++ src/schemas/reviews-schema.ts
@@ -1,13 +1,17 @@
 import { z } from "zod";
 
+const ratingSchema = z.object({
+  maintenance: z.number().max(5).min(1),
+  drivability: z.number().max(5).min(1),
+  comfort: z.number().max(5).min(1),
+  consumption: z.number().max(5).min(1),
+  general: z.number().max(5).min(1),
+});
+
 export const reviewSchema = z.object({
   title: z.string().max(50),
   text: z.string().max(1000),
-  maintenance: z.optional(z.number().max(5).min(1)),
-  drivability: z.optional(z.number().max(5).min(1)),
-  comfort: z.optional(z.number().max(5).min(1)),
-  consumption: z.optional(z.number().max(5).min(1)),
-  general: z.optional(z.number().max(5).min(1)),
   tags: z.optional(z.array(z.string().uuid()).max(3)),
   carId: z.string().uuid(),
+  x: z.optional(ratingSchema),
 });
